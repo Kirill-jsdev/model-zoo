60a6b644c3fcf1dd42e352af5a2159742fec1d28
Kirill <jsdevzk@gmail.com>
2023-05-02 15:14:40 +0200

    Added colorization functionality to ModelZooBrowser

--- src/App.tsx
+++ src/App.tsx
@@ -3,33 +3,27 @@ import './App.css'
 import ModelZooBrowser from './ModelZooBrowser/ModelZooBrowser'
 import { useLogin, useGetModel, useGetVersions, useGetDataset } from './API/api'
 
-// const JOB_ID = 'bd4f208e-808d-492d-a4c0-550e42924445'
-// const VIRSIONS_ID = '5522a4b9-ecc4-48d1-aa16-fb1563bf2a7e'
+const JOB_ID = 'bd4f208e-808d-492d-a4c0-550e42924445'
+const DATASET_ID = '5522a4b9-ecc4-48d1-aa16-fb1563bf2a7e'
 
 const JOB_ID_TWO = '8a743fef-4f68-4152-83b4-3da9b749732d'
-const VIRSIONS_ID_TWO = 'b5a4cd8c-b7df-431b-869c-5f0f914774b6' //not changed yet
-const DATASET_ID_TWO = 'b5a4cd8c-b7df-431b-869c-5f0f914774b6'
+const DATASET_ID_TWO = 'b5a4cd8c-b7df-431b-869c-5f0f914774b6' //not changed yet
 
 
 function App() {
 
     const { user } = useLogin()
-
     const {model} = useGetModel(JOB_ID_TWO, user?.token)
+    const {versions} = useGetVersions(DATASET_ID_TWO, user?.token)
 
-    // console.log('Model In my APP', model)
+    let variables: any
 
-    const {versions} = useGetVersions(VIRSIONS_ID_TWO, user?.token)
-
-    // console.log('versions', versions)
-
-    const {dataset} = useGetDataset(DATASET_ID_TWO, user?.token)
-
-    console.log('dataset', dataset)
+    if (versions && versions[0]?.variables)
+      variables = versions[0].variables
 
   return (
     <div className="App">
-        {model && versions && <ModelZooBrowser model={model} versions={versions} />}
+        {model && variables && <ModelZooBrowser model={model} variables={variables} />}
     </div>
   );
 }
--- src/ModelZooBrowser/ModelZooBrowser.tsx
+++ src/ModelZooBrowser/ModelZooBrowser.tsx
@@ -10,7 +10,7 @@ import ModelZooBrowserWrapper from './ModelZooBrowserWrapper'
 
 type ViewDataType = 'treemap' | 'table'
 
-const ModelZooBrowser: React.FC<any> = ({model, versions}) => {
+const ModelZooBrowser: React.FC<any> = ({model, variables}) => {
 
     const [viewDataAs, setViewDataAs] = useState<ViewDataType>('treemap')
     const models = model?.model?.normalBehaviorModel?.models
@@ -42,72 +42,3 @@ const ModelZooBrowser: React.FC<any> = ({model, versions}) => {
 }
 
 export default ModelZooBrowser
-
-const variables = [ //data from VIRSIONS endpoint
-  {
-      "minimumValue": 1,
-      "name": "cnt",
-      "maximumValue": 249,
-      "firstTimestamp": "2010-12-31T17:00:00.000Z",
-      "lastTimestamp": "2011-01-31T16:00:00.000Z",
-      "orderInTable": 2,
-      "type": "Numerical",
-      "missingObservations": 0,
-      "averageValue": 52.24731182795699,
-  },
-  {
-      "minimumValue": 0,
-      "name": "windspeed",
-      "maximumValue": 5821,
-      "firstTimestamp": "2010-12-31T17:00:00.000Z",
-      "lastTimestamp": "2011-01-31T16:00:00.000Z",
-      "orderInTable": 3,
-      "type": "Numerical",
-      "missingObservations": 0,
-      "averageValue": 1857.9059139784947,
-  },
-  {
-      "minimumValue": 21,
-      "name": "hum_p",
-      "maximumValue": 100,
-      "firstTimestamp": "2010-12-31T17:00:00.000Z",
-      "lastTimestamp": "2011-01-31T16:00:00.000Z",
-      "orderInTable": 4,
-      "type": "Numerical",
-      "missingObservations": 0,
-      "averageValue": 59.20161290322581,
-  },
-  {
-      "minimumValue": 127,
-      "name": "temp_o",
-      "maximumValue": 2962,
-      "firstTimestamp": "2010-12-31T17:00:00.000Z",
-      "lastTimestamp": "2011-01-31T16:00:00.000Z",
-      "orderInTable": 5,
-      "type": "Numerical",
-      "missingObservations": 0,
-      "averageValue": 1346.9932795698924,
-  },
-  {
-      "minimumValue": -154832,
-      "name": "atemp_o",
-      "maximumValue": -16,
-      "firstTimestamp": "2010-12-31T17:00:00.000Z",
-      "lastTimestamp": "2011-01-31T16:00:00.000Z",
-      "orderInTable": 6,
-      "type": "Numerical",
-      "missingObservations": 0,
-      "averageValue": -73195.42876344085,
-  },
-  {
-      "minimumValue": 0,
-      "name": "holiday",
-      "maximumValue": 1,
-      "firstTimestamp": "2010-12-31T17:00:00.000Z",
-      "lastTimestamp": "2011-01-31T16:00:00.000Z",
-      "orderInTable": 7,
-      "type": "Boolean",
-      "missingObservations": 0,
-      "averageValue": 0.04838709677419355,
-  }
-]
