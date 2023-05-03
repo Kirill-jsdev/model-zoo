473d5d9e856f603bc2d48c54842d64f1749e6540
Kirill <jsdevzk@gmail.com>
2023-05-03 12:50:45 +0200

    imported more files

---
+++ src/ModelZooBrowser/Insights/DetermineScale.tsx
@@ -0,0 +1,72 @@
+import React, { useEffect, useMemo } from 'react'
+import styled from 'styled-components'
+import { color as coreColor } from '../Utilities/color'
+import { useSelectedDatasetVersion, DataAvailabilityScale } from 'src/context/Dataset'
+import { VariableOffsets } from 'src/context/ForecastingResults'
+import { Select } from 'src/components/atoms'
+
+interface DetermineScaleProps {
+  offsets: VariableOffsets[]
+  scale: DataAvailabilityScale
+  onChange: (scale: DataAvailabilityScale) => void
+}
+
+const scaleOptions = [
+  { option: `Samples`, value: DataAvailabilityScale.SAMPLE },
+  { option: `Seconds`, value: DataAvailabilityScale.SECONDS },
+  { option: `Minutes`, value: DataAvailabilityScale.MINUTES },
+  { option: `Quarter-hours`, value: DataAvailabilityScale.QUARTER_HOUR },
+  { option: `Hours`, value: DataAvailabilityScale.HOUR },
+  { option: `Days`, value: DataAvailabilityScale.DAY },
+  { option: `7 days`, value: DataAvailabilityScale.WEEK },
+  { option: `30 days`, value: DataAvailabilityScale.MONTH },
+]
+
+const SCALE_MAX = 10
+
+function determineScale(msDistance: number): DataAvailabilityScale {
+  if (msDistance > DataAvailabilityScale.WEEK * SCALE_MAX) return DataAvailabilityScale.MONTH
+  if (msDistance > DataAvailabilityScale.DAY * SCALE_MAX) return DataAvailabilityScale.WEEK
+  if (msDistance > DataAvailabilityScale.HOUR * SCALE_MAX) return DataAvailabilityScale.DAY
+  if (msDistance > DataAvailabilityScale.QUARTER_HOUR * SCALE_MAX) return DataAvailabilityScale.HOUR
+  if (msDistance > DataAvailabilityScale.MINUTES * SCALE_MAX) return DataAvailabilityScale.QUARTER_HOUR
+  if (msDistance > DataAvailabilityScale.SECONDS * SCALE_MAX) return DataAvailabilityScale.MINUTES
+  if (msDistance > DataAvailabilityScale.SECONDS) return DataAvailabilityScale.SECONDS
+  return DataAvailabilityScale.SAMPLE
+}
+
+export const DetermineScale: React.FC<DetermineScaleProps> = ({ offsets, scale, onChange }) => {
+  const { originalSamplingPeriod = 0 } = useSelectedDatasetVersion()
+
+  const maxOffset = useMemo(() => {
+    return offsets.reduce((acc, { usedOffsets }) => {
+      if (!usedOffsets) return acc
+      const { from, to } = usedOffsets
+      const max = Math.max(Math.abs(from), Math.abs(to))
+      return max > acc ? max : acc
+    }, 0)
+  }, [offsets])
+
+  useEffect(() => onChange(determineScale(originalSamplingPeriod * maxOffset)), [originalSamplingPeriod, maxOffset])
+
+  const handleScaleChange = (e: React.ChangeEvent<{ value: unknown }>) =>
+    onChange(e.target.value as DataAvailabilityScale)
+
+  return (
+    <ScaleSelect
+      label="Scale"
+      options={scaleOptions}
+      value={scale ?? ''}
+      onChange={handleScaleChange}
+      focusColor={coreColor.shades.smoke.dd}
+    />
+  )
+}
+
+const ScaleSelect = styled(Select)`
+  margin: 1rem 0 1.25rem 0.1rem;
+
+  .MuiSelect-selectMenu {
+    padding: 0.75rem;
+  }
+`
---
+++ src/ModelZooBrowser/Insights/Scale.tsx
@@ -0,0 +1,81 @@
+import React, { useMemo } from 'react'
+import { SliderProps } from '@material-ui/core'
+import { DatasetVariable } from 'src/Types'
+import { DataAvailabilityScale } from 'src/context/Dataset'
+import { VariableOffsets } from 'src/context/ForecastingResults/hooks'
+import { Scale as ScaleComponent } from 'src/components/organisms/Scale'
+
+interface ScaleProps extends VariableOffsets, Omit<DatasetVariable, 'variable'> {
+  scale: DataAvailabilityScale
+  scaleRatio: number
+  sliderProps?: SliderProps
+}
+
+const ROUND_FACTOR = 100
+
+const SCALE_PREFIX_MAP: Record<DataAvailabilityScale, string> = {
+  [DataAvailabilityScale.SAMPLE]: 'S',
+  [DataAvailabilityScale.SECONDS]: 's',
+  [DataAvailabilityScale.MINUTES]: 'm',
+  [DataAvailabilityScale.QUARTER_HOUR]: 'qh',
+  [DataAvailabilityScale.HOUR]: 'h',
+  [DataAvailabilityScale.DAY]: 'D',
+  [DataAvailabilityScale.WEEK]: 'W',
+  [DataAvailabilityScale.MONTH]: 'M',
+}
+
+function plusPrefix(value: string | number): string {
+  return Number(value) < 0 ? `${value}` : `+${value}`
+}
+
+function label(usedOffsets: VariableOffsets['usedOffsets']): string {
+  if (!usedOffsets) return ''
+  const { from, to } = usedOffsets
+  return `S${plusPrefix(from)} : S${plusPrefix(to)}`
+}
+
+function tooltipLabel(scale: DataAvailabilityScale, value?: number[]): string {
+  if (!value) return ''
+
+  const scalePrefix = SCALE_PREFIX_MAP[scale]
+  const first = Math.round(value[0] * ROUND_FACTOR) / ROUND_FACTOR
+  const second = Math.round(value[1] * ROUND_FACTOR) / ROUND_FACTOR
+  if (first === second) return `${scalePrefix}${plusPrefix(first)}`
+  return `${scalePrefix}${plusPrefix(first)} : ${scalePrefix}${plusPrefix(second)}`
+}
+
+export const Scale: React.FC<ScaleProps> = ({
+  usedOffsets,
+  sliderProps,
+  scale,
+  scaleRatio,
+  variable,
+  color,
+  textColor,
+}) => {
+  const scaledUnusedOffsets = useMemo(() => {
+    if (!usedOffsets) return
+    const { from, to } = usedOffsets
+    return { from: from * scaleRatio, to: to * scaleRatio }
+  }, [scaleRatio, usedOffsets])
+
+  const value = useMemo(() => {
+    return scaledUnusedOffsets ? [scaledUnusedOffsets.from, scaledUnusedOffsets.to] : undefined
+  }, [scaledUnusedOffsets])
+
+  const tooltipLabelFromValue = useMemo(() => tooltipLabel(scale, value), [scale, value])
+
+  return (
+    <ScaleComponent
+      name={variable}
+      label={label(usedOffsets)}
+      tooltipLabel={tooltipLabelFromValue}
+      color={textColor}
+      $backgroundColor={color}
+      sliderProps={{
+        value: scaledUnusedOffsets ? [scaledUnusedOffsets.from, scaledUnusedOffsets.to] : undefined,
+        ...sliderProps,
+      }}
+    />
+  )
+}
