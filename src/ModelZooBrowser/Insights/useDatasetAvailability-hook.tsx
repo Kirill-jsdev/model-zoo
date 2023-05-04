030a6c4223f7ef156bf1b332f84f8a73d8688049
Kirill <jsdevzk@gmail.com>
2023-05-04 11:45:51 +0200

    imported useDataAvailability hook, which returns scale property. This commit takes one file and should not break anything

---
+++ src/ModelZooBrowser/Insights/useDatasetAvailability-hook.tsx
@@ -0,0 +1,111 @@
+import {useState, useEffect, useMemo} from 'react'
+
+export enum DataAvailabilityScale {
+    SAMPLE = 1,
+    SECONDS = 1000,
+    MINUTES = 60_000,
+    QUARTER_HOUR = 900_000,
+    HOUR = 3_600_000,
+    DAY = 86_400_000,
+    WEEK = 604_800_000,
+    MONTH = 2_592_000_000,
+}
+
+export const SCALE_MAX = 10
+
+export const useDataAvailability: any = (selectedVersion: any, target: any, originalSamplingPeriod: number) => {
+    // const { selectedVersion, target, originalSamplingPeriod = 0 } = useSelectedDatasetVersion()
+    const [scale, setScale] = useState<DataAvailabilityScale>(DataAvailabilityScale.SAMPLE)
+
+    const targetLatest = useMemo(() => (target?.lastTimestamp ? new Date(target.lastTimestamp).getTime() : undefined), [target])
+
+    const availabilities: VariableAvailability[] = useMemo(() => {
+      if (!selectedVersion || !target) return []
+      return getVariableAvailabilities(selectedVersion.variables)
+    }, [selectedVersion, target])
+
+    useEffect(() => {
+      if (!targetLatest) return
+      const smallestDiff = smallestDifferenceWithTarget(availabilities, targetLatest)
+
+      if (smallestDiff !== Number.POSITIVE_INFINITY) {
+        setScale(determineScale(smallestDiff))
+        return
+      }
+
+      setScale(determineScale(originalSamplingPeriod * SCALE_MAX))
+    }, [availabilities, targetLatest])
+
+    const variables: ScaledAvailability[] = useMemo(() => {
+      if (!targetLatest) return []
+      return availabilities.map((availability) => {
+        const difference = availability.latestTimestamp ? (availability.latestTimestamp - targetLatest) : 0
+        const relativeValue = difference / originalSamplingPeriod
+        const scaleToUse = scale === DataAvailabilityScale.SAMPLE ? originalSamplingPeriod : scale
+        const scaledRelativeValue = Number((difference / scaleToUse).toFixed(1))
+
+        return { ...availability, relativeValue, scaledRelativeValue }
+      })
+    }, [availabilities, scale, targetLatest, originalSamplingPeriod])
+
+    console.log('VARIABLESSSSSSS', variables)
+    console.log('SCALE', scale)
+
+    return { scale, variables, setScale }
+  }
+
+//TYPES
+
+export type ScaledAvailability = VariableAvailability & {
+    relativeValue: number
+    scaledRelativeValue: number
+}
+
+export type VariableAvailability = {
+    name: string
+    latestTimestamp?: number
+}
+
+export type UseDataAvailability = () => {
+    scale: DataAvailabilityScale
+    variables: ScaledAvailability[]
+    setScale: (scale: DataAvailabilityScale) => void
+}
+
+export type Variable = {
+    name: string
+    type?: 'Numerical' | 'Boolean'
+    firstTimestamp?: string
+    lastTimestamp?: string
+    minimumValue?: number
+    maximumValue?: number
+    averageValue?: number
+    missingObservations?: number | null
+}
+
+//HELPER FUNCTIONS
+function getVariableAvailabilities(variables: Variable[]): VariableAvailability[] {
+    return variables.map(({ name, lastTimestamp }) => lastTimestamp ? ({
+      name,
+      latestTimestamp: new Date(lastTimestamp).getTime()
+    }) : ({name}))
+}
+
+function smallestDifferenceWithTarget(availabilities: VariableAvailability[], targetLatest: number): number {
+    return availabilities.reduce<number>((prev, { latestTimestamp }) => {
+      const difference = latestTimestamp ? Math.abs(latestTimestamp - targetLatest) : 0
+      const isSmaller = (difference > 0) && (difference < prev)
+      return isSmaller ? difference : prev
+    }, Number.POSITIVE_INFINITY)
+}
+
+function determineScale(smallestDifference: number): DataAvailabilityScale {
+    if (smallestDifference > DataAvailabilityScale.WEEK * SCALE_MAX) return DataAvailabilityScale.MONTH
+    if (smallestDifference > DataAvailabilityScale.DAY * SCALE_MAX) return DataAvailabilityScale.WEEK
+    if (smallestDifference > DataAvailabilityScale.HOUR * SCALE_MAX) return DataAvailabilityScale.DAY
+    if (smallestDifference > DataAvailabilityScale.QUARTER_HOUR * SCALE_MAX) return DataAvailabilityScale.HOUR
+    if (smallestDifference > DataAvailabilityScale.MINUTES * SCALE_MAX) return DataAvailabilityScale.QUARTER_HOUR
+    if (smallestDifference > DataAvailabilityScale.SECONDS * SCALE_MAX) return DataAvailabilityScale.MINUTES
+    if (smallestDifference > DataAvailabilityScale.SECONDS) return DataAvailabilityScale.SECONDS
+    return DataAvailabilityScale.SAMPLE
+}
