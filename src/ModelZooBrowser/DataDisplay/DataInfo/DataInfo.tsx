c9d5bd0d8fcca8aacfd93f264322cd4cb34b4ac9
Kirill <jsdevzk@gmail.com>
2023-05-10 11:04:47 +0200

    Implemented Model Index information display

--- src/ModelZooBrowser/DataDisplay/DataInfo/DataInfo.tsx
+++ src/ModelZooBrowser/DataDisplay/DataInfo/DataInfo.tsx
@@ -3,14 +3,17 @@ import { ModelZooBrowserContext } from '../../Context/ModelZooBrowserContextProv
 
 const DataInfo = () => {
 
-    const { onVisualizeAsChange } = useContext(ModelZooBrowserContext)
+    const { selectedModelIndex, selectedModelTerms, onVisualizeAsChange } = useContext(ModelZooBrowserContext)
 
     const handleSelectChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
         onVisualizeAsChange(e.target.value as 'treemap' | 'table')
     }
 
+    const modelDetailsText = selectedModelIndex ? `Model Index ${selectedModelIndex} (${selectedModelTerms?.length}) Terms` : 'Overview'
+
     return (
         <div className="data-info">
+            <h6 className='model-details-par'>{modelDetailsText}</h6>
             <select onChange={handleSelectChange} defaultValue='treemap' >
                 <option value="treemap">Treemap</option>
                 <option value="table">Table</option>
