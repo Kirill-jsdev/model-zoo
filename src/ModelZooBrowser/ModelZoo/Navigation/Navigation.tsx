008c5af440c039ffd1bdb55a8fee6b37cb5671f6
Kirill <jsdevzk@gmail.com>
2023-05-18 17:16:11 +0200

    Created Download functionality

--- src/ModelZooBrowser/ModelZoo/Navigation/Navigation.tsx
+++ src/ModelZooBrowser/ModelZoo/Navigation/Navigation.tsx
@@ -11,12 +11,29 @@ const Header = () => {
     onSelectedModelIndexChange(undefined)
   }
 
-  const downloadModelAsJson = (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
-    const anchorElement = e.target as HTMLAnchorElement
-    const dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(model))
-    anchorElement.setAttribute("href", dataStr)
-    anchorElement.setAttribute("download", "data.json")
-  }
+  // const downloadModelAsJson = (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+  //   const anchorElement = e.target as HTMLAnchorElement
+  //   const dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(model))
+  //   anchorElement.setAttribute("href", dataStr)
+  //   anchorElement.setAttribute("download", "data.json")
+  // }
+
+  const handleDownloadJSON = () => {
+    const json = JSON.stringify(model);
+    const blob = new Blob([json], { type: 'application/json' });
+    const url = URL.createObjectURL(blob);
+
+    const link = document.createElement('a');
+    link.href = url;
+    link.download = 'model.json';
+
+    // Programmatically click the link to start the download
+    link.click();
+
+    // Clean up by removing the link
+    URL.revokeObjectURL(url);
+    link.remove();
+  };
 
   return (
     <div className='nav-header'>
@@ -24,7 +41,7 @@ const Header = () => {
         <ArrowUpIcon />
         <h6>overview</h6>
       </div>
-      <a href='/' onClick={(e) => downloadModelAsJson(e)}><DownloadIcon /></a>
+      <div onClick={handleDownloadJSON} style={{width: 30, height: 30, cursor: 'pointer'}}><DownloadIcon /></div>
     </div>
   )
 }
