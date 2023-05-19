cb815065fa51b828fa8b10b878044e7fbb1171b0
Kirill <jsdevzk@gmail.com>
2023-05-19 11:40:31 +0200

    Styled download button

--- src/ModelZooBrowser/ModelZoo/Navigation/Navigation.tsx
+++ src/ModelZooBrowser/ModelZoo/Navigation/Navigation.tsx
@@ -11,28 +11,17 @@ const Header = () => {
     onSelectedModelIndexChange(undefined)
   }
 
-  // const downloadModelAsJson = (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
-  //   const anchorElement = e.target as HTMLAnchorElement
-  //   const dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(model))
-  //   anchorElement.setAttribute("href", dataStr)
-  //   anchorElement.setAttribute("download", "data.json")
-  // }
-
   const handleDownloadJSON = () => {
-    const json = JSON.stringify(model);
-    const blob = new Blob([json], { type: 'application/json' });
-    const url = URL.createObjectURL(blob);
+    const json = JSON.stringify(model)
+    const blob = new Blob([json], { type: 'application/json' })
+    const url = URL.createObjectURL(blob)
+    const link = document.createElement('a')
 
-    const link = document.createElement('a');
-    link.href = url;
-    link.download = 'model.json';
-
-    // Programmatically click the link to start the download
-    link.click();
-
-    // Clean up by removing the link
-    URL.revokeObjectURL(url);
-    link.remove();
+    link.href = url
+    link.download = 'model.json'
+    link.click()
+    URL.revokeObjectURL(url)
+    link.remove()
   };
 
   return (
@@ -41,7 +30,7 @@ const Header = () => {
         <ArrowUpIcon />
         <h6>overview</h6>
       </div>
-      <div onClick={handleDownloadJSON} style={{width: 30, height: 30, cursor: 'pointer'}}><DownloadIcon /></div>
+      <div className='mzb-download' onClick={handleDownloadJSON}><DownloadIcon /></div>
     </div>
   )
 }
